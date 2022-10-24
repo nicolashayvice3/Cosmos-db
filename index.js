386b72c8489d51f0687f3c008d35463b28bdb00b
nicolashayvice3 <nicolashayvice@gmail.com>
2022-10-25 10:51:16 +1300

    cosmosdb processes data but only within the same file

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ dotenv.config();
 
 import { CosmosClient } from "@azure/cosmos";
 
-const cosmosClient = new CosmosClient({ endpoint, key });
 
 const key = process.env.COSMOS_KEY;
 const endpoint = process.env.COSMOS_ENDPOINT;
@@ -16,6 +15,8 @@ const databaseName = `contoso_${timeStamp}`;
 const containerName = `products_${timeStamp}`;
 const partitionKeyPath = ["/categoryName"]
 
+const cosmosClient = new CosmosClient({ endpoint, key });
+
 const { database } = await cosmosClient.databases.createIfNotExists({ id: databaseName });
 console.log(`${database.id} database ready`);
 
@@ -27,3 +28,26 @@ const { container } = await database.containers.createIfNotExists({
 });
 console.log(`${container.id} container ready`);
 
+// interacting with data for the containers
+
+const cities = [
+    { id: "1", name: "Olympia", state: "WA", isCapitol: true },
+    { id: "2", name: "Redmond", state: "WA", isCapitol: false },
+    { id: "3", name: "Chicago", state: "IL", isCapitol: false }
+];
+
+for (const city of cities) {
+    container.items.create(city);
+}
+
+// examples of how to query the database container
+
+// await container.item("1").read();
+// await container.item("1").delete();
+
+const { resources } = await container.items
+  .query("SELECT * from c WHERE c.isCapitol = true")
+  .fetchAll();
+for (const city of resources) {
+  console.log(`${city.name}, ${city.state} is a capitol `);
+}
