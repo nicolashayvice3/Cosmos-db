b4cc5151055aa1ed7976e23482085410362d7bad
nicolashayvice3 <nicolashayvice@gmail.com>
2022-10-26 13:44:04 +1300

    rest api data adds to database

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
+require('dotenv').config();
 
-import { CosmosClient } from "@azure/cosmos";
-
+const { CosmosClient } = require("@azure/cosmos")
+const axios = require('axios')
 
 const key = process.env.COSMOS_KEY;
 const endpoint = process.env.COSMOS_ENDPOINT;
@@ -17,37 +16,49 @@ const partitionKeyPath = ["/categoryName"]
 
 const cosmosClient = new CosmosClient({ endpoint, key });
 
-const { database } = await cosmosClient.databases.createIfNotExists({ id: databaseName });
-console.log(`${database.id} database ready`);
+async function saveData(dataApi) {
+  console.log("save data", dataApi)
 
-const { container } = await database.containers.createIfNotExists({
-    id: containerName,
-    partitionKey: {
-        paths: partitionKeyPath
-    }
-});
-console.log(`${container.id} container ready`);
+  const { database } = await cosmosClient.databases.createIfNotExists({ id: databaseName });
+  console.log(`${database.id} database ready`);
+  
+  const { container } = await database.containers.createIfNotExists({
+      id: containerName,
+      partitionKey: {
+          paths: partitionKeyPath
+      }
+  });
+  console.log(`${container.id} container ready`);
+  
+  container.items.create(dataApi)
+  .catch(err => {
+    console.log("error saving: ", err)
+  })
 
-// interacting with data for the containers
-
-const cities = [
-    { id: "1", name: "Olympia", state: "WA", isCapitol: true },
-    { id: "2", name: "Redmond", state: "WA", isCapitol: false },
-    { id: "3", name: "Chicago", state: "IL", isCapitol: false }
-];
-
-for (const city of cities) {
-    container.items.create(city);
-}
-
-// examples of how to query the database container
-
-// await container.item("1").read();
-// await container.item("1").delete();
-
-const { resources } = await container.items
-  .query("SELECT * from c WHERE c.isCapitol = true")
+  // examples of how to query the database container
+  // container.item("1").read()
+  // await container.item("1").delete()
+  
+  const { resources } = await container.items
+  .query("SELECT * from c WHERE c.title = 'delectus aut autem'")
   .fetchAll();
-for (const city of resources) {
-  console.log(`${city.name}, ${city.state} is a capitol `);
+  console.log(` is it complete? ${dataApi.completed}`)
 }
+
+function apiCall() {
+    axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => {
+      const data = response.data;
+      console.log("data api", data)
+      return data
+    })
+    .then(data => {
+      console.log("then data api", data)
+      saveData(data)
+    })
+    .catch(err => {
+      console.log('Error: ', err);
+    })
+}
+
+apiCall()
